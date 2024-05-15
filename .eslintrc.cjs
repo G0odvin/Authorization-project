5ec16639d73ef8dc8888f9b5cd5f17474e8a4181
Novyk Anton <novyk.anton@gmail.com>
2024-05-15 11:50:03 +0300

    Registration form

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: 'true'
+    }
+  },
+  settings: {
+    react: { version: '18.2' },
+    'import/extensions': ['.js', '.jsx'],
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
